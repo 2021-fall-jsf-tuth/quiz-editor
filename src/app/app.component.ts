53cd20ab6d9419614c95bd58ff844c91b44117c8
aleebit <alee@madisoncollege.edu>
2021-11-02 11:50:05 -0500

    Quiz quizservice.ts and  app componant.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from './quiz.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'quiz-editor';
+
+constructor(
+  private quizSvc: QuizService
+) {}
+
+ngOnInit() {
+  const qs = this.quizSvc.loadQuizzes();
+  console.log(qs);
+}
 }
