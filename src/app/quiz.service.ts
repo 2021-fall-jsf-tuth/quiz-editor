e6d8654c0c3224d20e803ce9a0a63fdd659c728f
tpetts <tpetts@madisoncollege.edu>
2021-11-02 11:46:35 -0500

    Using Dependency Injection
    So that we can use the new service that we created and inject it into our web page component

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,4 +6,40 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
+  loadQuizzes = () => {
+    // creating a new array literal
+    const quizzesFromWeb = [
+      // with an object
+      {
+        name: 'Quiz 1'
+        // with another array literal holding...
+        , questions: [
+          // more objects!
+          {
+            name: 'Question 1'
+          }
+          , {
+            name: 'Question 2'
+          }
+        ]
+      }
+      , {
+        name: 'Quiz 2'
+        , questions: [
+          {
+            name: 'Question 1'
+          }
+          , {
+            name: 'Question 2'
+          }
+          , {
+            name: 'Question 3'
+          }
+        ]
+      }
+    ];
+
+    return quizzesFromWeb;
+  };
 }
