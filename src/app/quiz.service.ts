ece5000cc9867b385aa1510db7ee32453be8ceeb
MegablasterMan1 <83662971+MegablasterMan1@users.noreply.github.com>
2021-11-02 11:47:30 -0500

    Plumbing working!
    calling the service...

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,4 +6,26 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
+
+  loadQuizzes = () => {
+    const quizzesFromWeb = [
+
+      {name: 'Quiz 1',
+        questions: [
+          {name: 'Question 1', data: 'Data 1'},
+          {name: 'Question 2', data: 'Data 2'},
+          {name: 'Question 3', data: 'Data 3'}
+        ]
+      },
+
+      {name: 'Quiz 2',
+        questions: [
+          {name: 'Question 1', data: 'Data 1.2'}
+        ]
+      }
+    ];
+
+    return quizzesFromWeb;
+  };
 }
