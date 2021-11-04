c2c28344b6d772a5e8512a3c16042cd8fbc49112
tpetts <tpetts@madisoncollege.edu>
2021-11-04 11:04:59 -0500

    using interfaces and practicing binding
    Mapping things out :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from './quiz.service';
 
+interface QuizDisplay {
+  quizName: string;
+  quizQuestions: QuestionDisplay[]
+}
+
+// interfaces can utilize other interfaces
+interface QuestionDisplay {
+  questionName: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,15 +20,22 @@ export class AppComponent implements OnInit {
   title = 'quiz-editor';
 
   constructor(
+    // TS parameter property...
     private quizSvc: QuizService
   ) {}
 
+  // A life cycle event
   ngOnInit() {
     const qs = this.quizSvc.loadQuizzes();
     console.log(qs);  
     
-    this.quizzes = qs;
+    this.quizzes = qs.map(x => ({
+      quizName: x.name
+      , quizQuestions: x.questions.map(y => ({
+          questionName: y.name 
+        }))
+    }));
   }
 
-  quizzes: any[] = [];
+  quizzes: QuizDisplay[] = [];
 }
