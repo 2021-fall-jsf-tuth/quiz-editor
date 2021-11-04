ef49c368813713366692a83d2f8157bf27d7bd1f
unknown <lizziluebke89@gmail.com>
2021-11-04 11:20:16 -0500

    Got selected quiz working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from './quiz.service';
 
+interface QuizDisplay {
+    quizName: string;
+    quizQuestions: QuestionDisplay[]
+}
+
+interface QuestionDisplay {
+    questionName: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,15 +19,33 @@ export class AppComponent implements OnInit {
   title = 'quiz-editor';
 
   constructor(
+      //TS PARAMERTER PROPERTY..
     private quizSvc: QuizService
+
+    // shorthand for
+    // quizsvc: QuizService
   ) {}
 
   ngOnInit() {
     const qs = this.quizSvc.loadQuizzes();
-    console.log(qs);  
-    
-    this.quizzes = qs;
+    console.log(qs);
+
+    this.quizzes = qs.map(x => ({
+        quizName: x.name
+        , quizQuestions: x.questions.map(y => ({
+            questionName: y.name
+        }))
+    }));
   }
 
-  quizzes: any[] = [];
+  quizzes: QuizDisplay[] = [];
+
+
+  selectedQuiz: QuizDisplay | undefined = undefined;
+
+  selectQuiz = (quizToSelect: QuizDisplay) => {
+   this.selectedQuiz = quizToSelect;
+   console.log(quizToSelect); 
+  };
+
 }
