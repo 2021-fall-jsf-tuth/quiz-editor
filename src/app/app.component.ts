9dd5345bdc505558f0b7ad3e33eacf94f2b4fc6e
sushicat <alee@madisoncollege.edu>
2021-11-05 16:13:43 -0500

    Creating Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,4 +42,19 @@ export class AppComponent implements OnInit {
     console.log(quizToSelect);
     this.selectedQuiz = quizToSelect;
   };
+
+  addNewQuiz = () => {
+    const newQuiz: QuizDisplay =  {
+      quizName: "untitledQuiz"
+      , quizQuestions:[]
+    };
+
+    this.quizzes = [
+      ...this.quizzes
+      , newQuiz
+    ];
+
+    this.selectQuiz(newQuiz);
+  }
+
 }
