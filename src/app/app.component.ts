b3a087a09b93937d14090f356e5e0560dba2c708
jonahhhhhh <91928902+jonahhhhhh@users.noreply.github.com>
2021-11-23 11:08:50 -0600

    deleted quiz badge working
    displays how many quizzes will be deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -183,4 +183,12 @@ export class AppComponent implements OnInit {
     this.selectedQuiz = undefined
   }
 
+  getDeletedQuizzes = () => {
+    return this.quizzes.filter(x => x.markedForDelete)
+  }
+
+  get deletedQuizCount() {
+    return this.getDeletedQuizzes().length
+  }
+
 }
