6e5106909e988da4a8b11ace9c7303194866fd54
tsteelematc <tsteele@madisoncollege.edu>
2021-11-23 11:08:44 -0600

    Deleted quiz badge working...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -182,4 +182,13 @@ export class AppComponent implements OnInit {
     this.load();
     this.selectedQuiz = undefined;
   };
+
+  getDeletedQuizzes = () => {
+    return this.quizzes.filter(x => x.markedForDelete);
+  };
+
+  get deletedQuizCount() {
+    return this.getDeletedQuizzes().length;
+  }
+
 }
