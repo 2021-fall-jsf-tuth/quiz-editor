74b689e942a9e3003d15d8865cc4c81406a83b34
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-11-30 12:14:43 -0600

    Fix a few typos to fix the animations....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -94,7 +94,7 @@ export class AppComponent implements OnInit {
   selectQuiz = (quizToSelect: QuizDisplay) => {
     console.log(quizToSelect);
     this.selectedQuiz = quizToSelect;
-    this.detailsFromLeftState = "finalPositon"
+    this.detailsFromLeftState = "finalPosition"
   };
 
   addNewQuiz = () => {
